557892af4f859c873b03689a67dfa98f0334df34
garrychandra <garryalexanderchandra97@gmail.com>
2024-10-23 22:22:13 +0700

    dummy update

--- dummy.java
+++ dummy.java
@@ -5,40 +5,42 @@ import java.sql.Time;
 public class dummy {
     public static void main(String[] args) {
 
-        Presensi presensi = new Presensi(new Date(0), Status.HADIR);
-        PresensiStaff presensiStaff = new PresensiStaff(new Date(0), Status.HADIR,new Time(0)); 
+        Presensi presensi = new Presensi(new Date(0), 1);
+        PresensiStaff presensiStaff = new PresensiStaff(new Date(System.currentTimeMillis()), 0, new Time(System.currentTimeMillis()));
 
-        MataKuliah mataKuliah = new MataKuliah(12345,4,"pbo");
-        MatkulPilihan matkulPilihan = new MatkulPilihan(54321,4,"basdat",9);
+        MataKuliah mataKuliah = new MataKuliah(12345, 4, "pbo");
+        MatkulPilihan matkulPilihan = new MatkulPilihan(54321, 4, "basdat", 9);
 
-        MatkulAmbil matkulAmbil = new MatkulAmbil(mataKuliah,100, 100, 100);
+        MatkulAmbil matkulAmbil = new MatkulAmbil(mataKuliah, 100, 100, 100);
         matkulAmbil.getPresensi().add(presensi);
-        matkulAmbil.getPresensi().add(new Presensi(new Date(1), Status.ALPHA));
+        matkulAmbil.getPresensi().add(new Presensi(new Date(System.currentTimeMillis()), Status.ALPHA));
 
         MatkulAjar matkulAjar = new MatkulAjar(mataKuliah);
         matkulAjar.getPresensi().add(presensiStaff);
 
-        
         Sarjana sarjana = new Sarjana("garry", "dipatiukur", "jakarta 2 desember 1900", 123456, 654321, "informatika");
         sarjana.getMatkul().add(matkulAmbil);
-        sarjana.getMatkul().add(new MatkulAmbil(matkulPilihan,100,100,100));
-        
-        Magister magister = new Magister("garry", "dipatiukur", "jakarta 2 desember 1900", 123456, 654321, "informatika","tbd");
+        sarjana.getMatkul().add(new MatkulAmbil(matkulPilihan, 100, 100, 100));
+        sarjana.getMatkul().get(1).getPresensi().add(new Presensi(new Date(0),Status.HADIR));
+
+        Magister magister = new Magister("garry", "dipatiukur", "jakarta 2 desember 1900", 123456, 654321,
+                "informatika", "tbd");
         magister.getMatkul().add(matkulAmbil);
 
-        Doktor doktor = new Doktor("garry", "dipatiukur", "jakarta 2 desember 1900", 123456, 654321, "informatika","tbd");
+        Doktor doktor = new Doktor("garry", "dipatiukur", "jakarta 2 desember 1900", 123456, 654321, "informatika",
+                "tbd");
         doktor.setSidang1(100);
 
-        DosenTetap dosenTetap = new DosenTetap("garry", "dipatiukur", "jakarta 2 desember 1900", 123456, 654321, "informatika",1000);
+        DosenTetap dosenTetap = new DosenTetap("garry", "dipatiukur", "jakarta 2 desember 1900", 123456, 654321,
+                "informatika", 1000);
         dosenTetap.getMatkulAjar().add(matkulAjar);
-        
-        DosenHonorer dosenHonorer = new DosenHonorer("garry", "dipatiukur", "jakarta 2 desember 1900", 123456, 654321, "informatika",100);
+
+        DosenHonorer dosenHonorer = new DosenHonorer("garry", "dipatiukur", "jakarta 2 desember 1900", 123456, 654321,
+                "informatika", 100);
         dosenHonorer.getMatkulAjar().add(matkulAjar);
-        
+
         Karyawan karyawan = new Karyawan("garry", "dipatiukur", "jakarta 2 desember 1900", 123456, 654321, 1000);
         karyawan.getPresensiStaff().add(presensiStaff);
-       
-        
 
         System.out.println("Sarjana\n" + sarjana +
                 "\n\nMagister\n" + magister +
